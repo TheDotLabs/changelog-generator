d1996a0df355856d434980853e692cfdc7a7f3bd
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-04-04 08:37:29 +0530

    Generate files through tsc
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- dist/src/utils.js
+++ dist/src/utils.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseCommits = void 0;
 var parseCommits = function (output) {
+    var _a;
     var commits = output.split("@begin@");
     if (commits[0] === "") {
         commits.shift();
@@ -9,16 +10,22 @@ var parseCommits = function (output) {
     for (var _i = 0, commits_1 = commits; _i < commits_1.length; _i++) {
         var commit = commits_1[_i];
         var title = commit.split("@space@")[0];
-        var body = commit.split("@space@")[1].replace('@end@', '').split('\n');
+        var body = commit.split("@space@")[1].replace('@end@', '');
+        var bodyLines = body.split('\n');
         console.log("- " + title.trim() + "  ");
-        for (var _a = 0, body_1 = body; _a < body_1.length; _a++) {
-            var bodyLine = body_1[_a];
+        for (var _b = 0, bodyLines_1 = bodyLines; _b < bodyLines_1.length; _b++) {
+            var bodyLine = bodyLines_1[_b];
             if (!bodyLine.includes('Signed-off') && bodyLine !== "") {
-                if (bodyLine.includes('app.asana.com')) {
-                    console.log(("Asana Task: [" + bodyLine + "](" + bodyLine + ")").trim());
+                if (!title.includes('(#')) {
+                    var urlRegex = /(https?:\/\/[^ ]*)/g;
+                    var urls = (_a = bodyLine.match(urlRegex)) !== null && _a !== void 0 ? _a : [];
+                    var newBodyLine = bodyLine;
+                    for (var _c = 0, urls_1 = urls; _c < urls_1.length; _c++) {
+                        var url = urls_1[_c];
+                        newBodyLine = newBodyLine.replace(url, "[" + url + "](" + url + ")");
+                    }
+                    console.log(newBodyLine.trim());
                 }
-                else if (!title.includes('(#'))
-                    console.log(bodyLine.trim());
             }
         }
     }
