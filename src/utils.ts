facdc272ed0e7c4a541eb5b5e84f836e9e39bc87
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-04-04 08:36:56 +0530

    Fix #1 Incorrect link format
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,26 @@
 export const parseCommits = function (output: String) {
-    const commits = output.split("@begin@");
-    if (commits[0] === "") {
-        commits.shift();
-    }
+  const commits = output.split("@begin@");
+  if (commits[0] === "") {
+    commits.shift();
+  }
 
-    for (const commit of commits) {
-        const title = commit.split("@space@")[0];
-        const body = commit.split("@space@")[1].replace('@end@', '').split('\n');
-        console.log(`- ${title.trim()}  `);
-        for (const bodyLine of body) {
-            if (!bodyLine.includes('Signed-off') && bodyLine !== "") {
-                if (bodyLine.includes('app.asana.com')) {
-                    console.log(`Asana Task: [${bodyLine}](${bodyLine})`.trim());
-                } else if (!title.includes('(#'))
-                    console.log(bodyLine.trim());
-            }
+  for (const commit of commits) {
+    const title = commit.split("@space@")[0];
+    const body = commit.split("@space@")[1].replace('@end@', '');
+    const bodyLines = body.split('\n');
+    console.log(`- ${ title.trim() }  `);
+    for (const bodyLine of bodyLines) {
+      if (!bodyLine.includes('Signed-off') && bodyLine !== "") {
+        if (!title.includes('(#')) {
+          const urlRegex = /(https?:\/\/[^ ]*)/g;
+          const urls = bodyLine.match(urlRegex) ?? [];
+          let newBodyLine: string = bodyLine;
+          for (const url of urls) {
+            newBodyLine = newBodyLine.replace(url, `[${ url }](${ url })`)
+          }
+          console.log(newBodyLine.trim());
         }
+      }
     }
+  }
 }
